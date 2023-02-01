775246a35f242f50046fc3c8e01ff78f137b89c7
marynghiem <marynghiem46@gmail.com>
2023-02-01 12:58:06 -0800

    added content inside register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
-const Register = () => {};
+const Register = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Mary's Chat</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input type="file" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
