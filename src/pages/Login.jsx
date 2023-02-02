0d74c943256df51cb30f28e5896e3e6c0b9cb8fd
marynghiem <marynghiem46@gmail.com>
2023-02-02 12:30:38 -0800

    added content for login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Add from "../images/addAvatar.png";
 
 const Login = () => {
-  return;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Mary's Chat</span>
+        <span className="title">Login</span>
+        <form>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign In</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
