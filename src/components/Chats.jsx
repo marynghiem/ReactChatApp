2f8a75c9995e1d53bb380d18fa2865efe25f2f68
marynghiem <marynghiem46@gmail.com>
2023-02-03 15:48:35 -0800

    styled chats component

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
-const Chats = () => {};
+const Chats = () => {
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://media.licdn.com/dms/image/D5635AQFOcceVKb3VZg/profile-framedphoto-shrink_400_400/0/1675297581656?e=1676070000&v=beta&t=gWh3-8326aoU4UGL3O0mRBcVO3y8NnnQWQAGTORVL6o"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Mary Nghiem</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Chats;
