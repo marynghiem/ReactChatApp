33ee5acaeebd22961113c3241ff5e78df1ed9973
marynghiem <marynghiem46@gmail.com>
2023-02-03 14:45:51 -0800

    finished styled search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,10 +4,13 @@ const Search = () => {
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" />
+        <input type="text" placeholder="Find a User" />
       </div>
       <div className="userChat">
-        <img src="" alt="" />
+        <img
+          src="https://media.licdn.com/dms/image/D5635AQFOcceVKb3VZg/profile-framedphoto-shrink_400_400/0/1675297581656?e=1676070000&v=beta&t=gWh3-8326aoU4UGL3O0mRBcVO3y8NnnQWQAGTORVL6o"
+          alt=""
+        />
         <div className="userChatInfo">
           <span>Mary Nghiem</span>
         </div>
