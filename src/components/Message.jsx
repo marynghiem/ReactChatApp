4cb3604c28e01c4f650311e506cab4949dd5759d
marynghiem <marynghiem46@gmail.com>
2023-02-06 15:02:38 -0800

    made app more responsive

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
         <img
           src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.18169-9/12242994_882022921905208_2410741588434389600_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=I8eAc403Ug0AX_1V9Xp&_nc_ht=scontent-sjc3-1.xx&oh=00_AfBRsgldJjHLTywca49s1e86Z2EWLKEUigNsTs7y4sNhtg&oe=6408F77C"
