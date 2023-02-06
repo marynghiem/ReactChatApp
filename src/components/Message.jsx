6d069399264d5193d26d77d042e0de2a67579bfb
marynghiem <marynghiem46@gmail.com>
2023-02-06 14:13:23 -0800

    styled the message sent

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Message = () => {
-  return <div className="message">Message</div>;
+  return (
+    <div className="message">
+      <div className="messageInfo">
+        <img
+          src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.18169-9/12242994_882022921905208_2410741588434389600_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=I8eAc403Ug0AX_1V9Xp&_nc_ht=scontent-sjc3-1.xx&oh=00_AfBRsgldJjHLTywca49s1e86Z2EWLKEUigNsTs7y4sNhtg&oe=6408F77C"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/277566717_1834953956700569_3868462168836889432_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=0gznqXHoQjgAX_P9sYf&_nc_ht=scontent-sjc3-1.xx&oh=00_AfA8zb_ksu_mBsP7FKUZVF-J_29EuZEibGkeu59vIb4L-w&oe=63E63458" />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
