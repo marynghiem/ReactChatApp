0e81c139ccf130aa0007103c7e197378662dec35
marynghiem <marynghiem46@gmail.com>
2023-02-09 14:14:35 -0800

    import items so that we can query data using firebase

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
+import { collection, query, where, getDocs, setDoc, doc, updateDoc, serverTimestamp, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
 
+  const handleSearch = () => {};
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Find a User" />
+        <input
+          type="text"
+          placeholder="Find a User"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
       <div className="userChat">
         <img
