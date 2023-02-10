460d47abccf8d5d074be282c79d529b3b2e45791
marynghiem <marynghiem46@gmail.com>
2023-02-09 17:01:26 -0800

    create chat context

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,21 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../firebase";
+import { doc, onSnapshot } from "firebase/firestore";
 
 const Chats = () => {
   const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  const handleSelect = (u) => {
+    dispatch({ type: "CHANGE_USER", payload: u });
+  };
+
   return (
     <div className="chats">
-      <div className="userChat">
-        <img
-          src="https://media.licdn.com/dms/image/D5635AQFOcceVKb3VZg/profile-framedphoto-shrink_400_400/0/1675297581656?e=1676070000&v=beta&t=gWh3-8326aoU4UGL3O0mRBcVO3y8NnnQWQAGTORVL6o"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Mary Nghiem</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      {Object.entries(chats)
+        ?.sort((a, b) => b[1].date - a[1].date)
+        .map((chat) => (
+          <div className="userChat" key={chat[0]} onClick={() => handleSelect(chat[1].userInfo)}>
+            <img src={chat[1].userInfo.photoURL} alt="" />
+            <div className="userChatInfo">
+              <span>{chat[1].userInfo.displayName}</span>
+              <p>{chat[1].lastMessage?.text}</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
